eb8bbc35519f76542d9f009efbdca9285a28f30f
unknown <patriciafelixx@outlook.com>
2020-10-23 10:46:49 -0300

    Initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface TitleProps {
+  text: string;
+}
+
+function Title(props: TitleProps) {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Title text='Hello' />
+    </div>
+  );
+}
+
+export default App;

d28952505f06d0a73d3851b20a048ae40b8ed9e6
unknown <patriciafelixx@outlook.com>
2020-10-23 22:17:08 -0300

    Stylizing the OrphanageMaps page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi'
+
+import MapMarker from '../images/map-marker.svg'
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
     return (
-        <h1>OrphanagesMap Page</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={MapMarker} alt="" />
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>SP</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <div>
+                <Link to="#" className="create-orphanage">
+                    <FiPlus size={32} color="fff" />
+                </Link>
+            </div>
+        </div>
     )
 }
 

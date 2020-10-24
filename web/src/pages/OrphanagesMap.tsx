a12cd88536ae327d106c31e8fb51d4f0e2bd4ca3
unknown <patriciafelixx@outlook.com>
2020-10-24 14:53:23 -0300

    Integrating Leaflet + Mapbox Api

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi'
 
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'
+
 import MapMarker from '../images/map-marker.svg'
 import '../styles/pages/orphanages-map.css';
 
@@ -19,6 +22,13 @@ function OrphanagesMap() {
                     <span>São Paulo</span>
                 </footer>
             </aside>
+
+            <Map center={[-23.5683422, -46.7206263]} zoom={15} style={{ width: '100%', height: '100%' }}>
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                <TileLayer
+                    url='http://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGF0cmljaWFmZWxpeHgiLCJhIjoiY2tnbng3amF0MDZpMDJ6bzhkd2RlYXIwaCJ9.EbN3b2FZT2gpt6I4R9lnZQ'
+                />
+            </Map>
 
             <div>
                 <Link to="#" className="create-orphanage">
